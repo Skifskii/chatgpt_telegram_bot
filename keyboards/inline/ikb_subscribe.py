82ad4b66c916520f318e39fa1b81eca0b16a6e1d
Skifskii <petya-koreshkov@yandex.ru>
2023-04-16 19:13:46 +0300

    changed an order of payments

--- keyboards/inline/ikb_subscribe.py
+++ keyboards/inline/ikb_subscribe.py
@@ -1,10 +1,13 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-ikb_subscribe = InlineKeyboardMarkup()
+ikb_subscribe = InlineKeyboardMarkup(row_width=2)
 
 btn_subscribe_gpt = InlineKeyboardButton(text='GPT', callback_data='btn_subscribe_gpt')
 ikb_subscribe.insert(btn_subscribe_gpt)
 
 btn_subscribe_vip = InlineKeyboardButton(text='VIP', callback_data='btn_subscribe_vip')
 ikb_subscribe.insert(btn_subscribe_vip)
+
+btn_back = InlineKeyboardButton(text='🔙 Отмена', callback_data='btn_back')
+ikb_subscribe.insert(btn_back)
--- utils/yookassa_api/new_payment.py
+++ utils/yookassa_api/new_payment.py
@@ -7,40 +7,37 @@ from yookassa import Payment
 from logs.log_all import log_all
 
 
-async def create_new_payment(price, subscription_name, user):
-    try:
-        new_payment = Payment.create({
-                "amount": {
-                    "value": f"{price}.00",
-                    "currency": "RUB"
+async def create_new_payment(price, subscription_name, email):
+    new_payment = Payment.create({
+            "amount": {
+                "value": f"{price}.00",
+                "currency": "RUB"
+            },
+            "confirmation": {
+                "type": "redirect",
+                "return_url": "https://t.me/chatgpt_davinci_aibot"
+            },
+            "capture": True,
+            "description": subscription_name,
+            "receipt": {
+                "customer": {
+                    "email": email
                 },
-                "confirmation": {
-                    "type": "redirect",
-                    "return_url": "https://t.me/chatgpt_davinci_aibot"
-                },
-                "capture": True,
-                "description": subscription_name,
-                "receipt": {
-                    "customer": {
-                        "email": user.email
-                    },
-                    "items": [
-                        {
-                            "description": subscription_name,
-                            "quantity": "1",
-                            "amount": {
-                                "value": f"{price}.00",
-                                "currency": "RUB"
-                            },
-                            "vat_code": "1"
-                        }
-                    ]
-                }
-            }, uuid.uuid4())
-        payment_data = json.loads(new_payment.json())
-        return payment_data
-    except Exception as error:
-        await log_all('create_new_payment', 'error', user.user_id, user.firstname, error)
+                "items": [
+                    {
+                        "description": subscription_name,
+                        "quantity": "1",
+                        "amount": {
+                            "value": f"{price}.00",
+                            "currency": "RUB"
+                        },
+                        "vat_code": "1"
+                    }
+                ]
+            }
+        }, uuid.uuid4())
+    payment_data = json.loads(new_payment.json())
+    return payment_data
 
 
 async def check_payment_status(payment_data, user):
